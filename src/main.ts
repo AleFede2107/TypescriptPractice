efd75c776c21ac5483a1e46f8ec5a2942ee01ba8
Federico <alegriafederico0@gmail.com>
2024-09-02 18:29:23 -0600

    :sparkles: feat: Intro Typescript

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,28 @@ let asistencia:boolean = false
 
 let today:Date = new Date()
 
-console.log(fullname)
+//arrays 
+let language:string[] = ['PHP', 'JS', 'Go']
+let years:Array<number> = [2020,2021,2022,2023]
+
+//Tuplas
+const tuplas:[string, boolean, number] = ['Hola', true, 2024]
+
+//Enum
+
+enum Roles {
+    Manager,
+    Editor,
+    Viewer
+}
+
+let userRole:Roles = Roles.Manager
+
+
+
+/* console.log(fullname)
 console.log(address)
 console.log(edad)
 console.log(alive)
 console.log(asistencia)
-console.log(today)
+console.log(today) */
