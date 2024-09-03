97b6b7be6430fc3a6359c0afbcbb67fa4ef2a7de
Federico <alegriafederico0@gmail.com>
2024-09-02 18:11:59 -0600

    :tada: initialize project

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+let fullname = "Federico"
+
+let address:string = 'El Salvador, San Salvador'
+
+let edad:number = 24
+
+let alive:boolean = true
+let asistencia:boolean = false
+
+let today:Date = new Date()
+
+console.log(fullname)
+console.log(address)
+console.log(edad)
+console.log(alive)
+console.log(asistencia)
+console.log(today)
