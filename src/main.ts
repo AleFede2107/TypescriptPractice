591da0e0606b4ebb3cea7c45659010a749daa17b
Federico <alegriafederico0@gmail.com>
2024-09-03 19:21:53 -0600

    :sparkles: feat: add notification handler

--- src/main.ts
+++ src/main.ts
@@ -1,94 +1,19 @@
-import axios from 'axios'
-// get characters
+const container:HTMLElement | null = document.getElementById('app')
 
-export interface IResponse {
-    info:    Info;
-    results: Result[];
-}
+const button:HTMLButtonElement = document.createElement('button')
 
-export interface Info {
-    count: number;
-    pages: number;
-    next:  string;
-    prev:  null;
-}
+button.textContent = 'Lanzar notificacion'
+container!.appendChild(button)
 
-export interface Result {
-    id:       number;
-    name:     string;
-    status:   Status;
-    species:  Species;
-    type:     string;
-    gender:   Gender;
-    origin:   Location;
-    location: Location;
-    image:    string;
-    episode:  string[];
-    url:      string;
-    created:  Date;
-}
-
-export enum Gender {
-    Female = "Female",
-    Male = "Male",
-    Unknown = "unknown",
-}
-
-export interface Location {
-    name: string;
-    url:  string;
-}
-
-export enum Species {
-    Alien = "Alien",
-    Human = "Human",
-}
-
-export enum Status {
-    Alive = "Alive",
-    Dead = "Dead",
-    Unknown = "unknown",
-}
-
-const getCharactersAxios = async():Promise<IResponse | never> => {
-    try {
-        const response = await axios.get<IResponse>(`https://rickandmortyapi.com/api/character`);
-        console.log(response.data)
-        return response.data
-    } catch (error) {
-        throw new Error('Error API Error')
+const notiHandler = async() => {
+    if(Notification.permission === 'granted'){
+        new Notification('Hi from my typescript Website')
+    }else {
+        const permission = await Notification.requestPermission()
+        if(permission === 'granted'){
+            new Notification('Gracias por permitirme entrar en tu vida')
+        }
     }
 }
 
-getCharactersAxios()
-
-
-const getCharactersFetch = async() => {
-    try {
-        const response = await fetch('https://rickandmortyapi.com/api/character')
-        const data:IResponse = await response.json()
-        return data
-    } catch (error) {
-        throw new Error('Error API ERROR')       
-    }
-}
-
-const renderCharacters = async() => {
-    const container = document.getElementById('app')
-    const data  = await getCharactersFetch()
-    if(data){
-        data.results.map(result => {
-            const div = document.createElement('div')
-            div.innerHTML = `
-                <h1> ${result.name}</h1>
-            `
-            container?.appendChild(div)
-        })
-    } else{
-        container!.innerHTML = `
-            <h1>TODO SE FUE A LA FREGADA T.T</h1>
-        `
-    }
-}
-
-renderCharacters();
+button.addEventListener('click', ():void => {console.log('notification'); notiHandler() })
